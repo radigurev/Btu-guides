5182464cf1583ba90c4c8e167b47aec6b1bb57f5
radigurev <radi.gurev@gmail.com>
2022-03-17 10:45:16 +0200

    rebuild slider

--- translate-offers-page/offers-page.js
+++ translate-offers-page/offers-page.js
@@ -1,12 +1,17 @@
-var container = document.getElementById('container')
+var container = document.getElementById('container');
 var slider = document.getElementById('slider');
+var containerDiv=[...document.getElementsByClassName('container')][0];
+var cards=[...document.getElementsByClassName('card')];
+var cards2=[...document.getElementsByClassName('card2')];
+var fsSlides=[...document.getElementsByClassName('slide')];
+var ssSlides=[...document.getElementsByClassName('slide2')];
 var slides = document.getElementsByClassName('slide').length;
 var buttons = document.getElementsByClassName('btn');
-
 
 var currentPosition = 0;
 var currentMargin = 0;
 var slidesPerPage = 0;
+var times=0;
 var slidesCount = slides - slidesPerPage;
 var containerWidth = container.offsetWidth;
 var prevKeyActive = false;
@@ -79,3 +84,93 @@ function slideLeft() {
         buttons[0].classList.remove('inactive');
     }
 };
+
+
+
+
+
+// Slider transition
+
+containerDiv.addEventListener('mouseover',e => {
+    cards.forEach(c => {
+        if(times===0){
+        c.classList.add('height-hundred');
+        c.childNodes[1].childNodes[1].classList.add('scale-one');
+        }
+    });
+    times++;
+});
+
+
+function changeSlider() {
+      cards=[...document.getElementsByClassName('card')];
+      cards2=[...document.getElementsByClassName('card2')];
+      fsSlides=[...document.getElementsByClassName('slide')];
+      ssSlides=[...document.getElementsByClassName('slide2')];
+
+       setTimeout(function () {
+            container.classList.add('hidden');
+            document.getElementById('container2').id='container'; 
+            container.id='container2';
+             container=document.getElementById('container');
+             document.getElementById('slider2').id='slider';
+             slider.id='slider2';
+             slider=document.getElementById('slider');
+       },500)
+    cards.forEach(c => {
+            c.childNodes[3].classList.add('hidden')
+           c.childNodes[1].childNodes[1].classList.add('animatedImg');
+            if(c.classList.contains('height-hundred')){
+                c.classList.remove('height-hundred');
+            }
+           setTimeout(function() {
+            c.classList.remove('card');
+            c.classList.add('card2');
+           },500);
+        });
+        fsSlides.forEach(s => {
+            s.classList.remove('slide');
+            s.classList.add('slide2');
+            
+        })
+    setTimeout(function() {
+        container.classList.remove('hidden');
+        slides = document.getElementsByClassName('slide').length;
+        buttons.forEach(b => {
+            b.classList.remove('btn');
+            b.classList.add('btn2');
+        })
+        document.getElementsByClassName('btn2').forEach(b => {
+            b.classList.remove('btn2');
+            b.classList.add('btn');
+        })
+        buttons = document.getElementsByClassName('btn');
+         currentPosition = 0;
+         currentMargin = 0;
+        slidesPerPage = 0;
+        slides=document.getElementsByClassName('slide').length;
+        slidesCount = slides - slidesPerPage;
+        containerWidth = container.offsetWidth;
+        prevKeyActive = false;
+        nextKeyActive = true;
+    },510);
+    console.log(ssSlides)
+    ssSlides.forEach(s => {
+        s.classList.remove('slide2');
+        s.classList.add('slide');
+    })
+    cards2.forEach(c => {
+        c.classList.remove('card2');
+        c.classList.add('card');
+        c.childNodes[1].childNodes[1].classList.remove('animatedImg');
+        if( c.childNodes[1].childNodes[1].classList.contains('scale-one')) {
+            c.childNodes[1].childNodes[1].classList.remove('scale-one');
+
+        }
+        setTimeout(function () {
+             c.classList.add('height-hundred');
+              c.childNodes[1].childNodes[1].classList.add('scale-one')
+             c.childNodes[3].classList.remove('hidden');
+            },600)
+    })
+}
